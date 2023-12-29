e2219019b8975e027b39f0d07282d9811687b2d6
Zenmaster28 <arend.teraa@gmail.com>
2023-12-29 11:10:24 -0400

    Route Preview Title change Hide next segment div until needed Delete unused copy files

--- pages/src/whereAmI.mjs
+++ pages/src/whereAmI.mjs
@@ -13,13 +13,11 @@ export async function main() {
 }
 
 async function processWatching(watching) {
+    
     if ((!routeInfo || watching.state.routeId != routeInfo.routeFullData.id) && !inProgress)
     {
         console.log("Getting segments on route")
-        inProgress = true;
-        //routeInfo = await coords.getSegmentsOnRoute(watching.state.courseId, watching.state.routeId, watching.state.eventSubgroupId) 
-        
-        //routeInfo = await coords.processRoute(watching.state.courseId, 2139708890)
+        inProgress = true;        
         if (watching.state.eventSubgroupId != 0) 
         {
             let sg = await common.rpc.getEventSubgroup(watching.state.eventSubgroupId)
@@ -29,15 +27,13 @@ async function processWatching(watching) {
                 routeInfo = await coords.processRoute(watching.state.courseId, watching.state.routeId, sg.laps ) 
             } else {
                 routeInfo = await coords.processRoute(watching.state.courseId, watching.state.routeId) 
-            }
-
-            //console.log(sg)
+            }         
             
         } else {
             routeInfo = await coords.processRoute(watching.state.courseId, watching.state.routeId) 
         }
         console.log(routeInfo)   
-        //debugger    
+        
         inProgress = false;
     }
     else {
@@ -46,17 +42,9 @@ async function processWatching(watching) {
         deltas.push(distDelta);
         if (deltas.length > 20)
         {            
-            deltas.shift();
-            //debugger
-        }
-        //debugger
+            deltas.shift();            
+        }        
         console.log(xCoord, watching.state.eventDistance, deltas.reduce((a, b) => a + b, 0) / deltas.length);
     }
 }
 
-const sum = deltas.reduce((a, b) => a + b, 0);
-const avg = (sum / deltas.length) || 0;
-
-function average(nums) {
-    return nums.reduce((a, b) => (a + b) / nums.length);
-}
