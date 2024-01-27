3e5f06f2a6d1bd27090eff1bdec07ae880cdfd34
Zenmaster28 <arend.teraa@gmail.com>
2024-01-27 08:21:40 -0400

    Add data fields to Sauce fields list

---
+++ pages/src/data-fields.js
@@ -0,0 +1,10 @@
+import('/pages/src/fields.mjs').then(module => {
+    module.fieldGroupNames['segments'] = 'Segments';       
+    module.fields.push({
+            group: 'Segments',
+            id: 'el-segment-next',
+            value: x => x.segmentData.nextSegment.name ? x.segmentData.nextSegment.name + ": " + x.segmentData.nextSegment.distanceToGo : '',
+            key: x => (x && x.athlete) ? '' : 'Next Segment',
+            unit: x => x.segmentData.nextSegment.distanceToGoUnits ? x.segmentData.nextSegment.distanceToGoUnits : ''
+    })
+});
