651a2cb2d60e3c3fe6925e3b3739a3359f54f2dd
Zenmaster28 <arend.teraa@gmail.com>
2024-04-16 12:57:22 -0300

    use segment displayName if available

--- pages/src/data-fields.js
+++ pages/src/data-fields.js
@@ -3,7 +3,7 @@ import('/pages/src/fields.mjs').then(module => {
     module.fields.push({
             group: 'Segments',
             id: 'el-segment-next',
-            value: x => x.segmentData.nextSegment.name ? x.segmentData.nextSegment.name + ": " + x.segmentData.nextSegment.distanceToGo : '',
+            value: x => x.segmentData.nextSegment.name ? (x.segmentData.nextSegment.displayName ?? x.segmentData.nextSegment.name) + ": " + x.segmentData.nextSegment.distanceToGo : '',
             key: x => (x && x.athlete) ? '' : 'Next Segment',
             unit: x => x.segmentData.nextSegment.distanceToGoUnits ? x.segmentData.nextSegment.distanceToGoUnits : ''
     })
