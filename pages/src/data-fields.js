fe606f565b8f85f7d8e68c95f72dd71706c2e53f
Arend teRaa <arend.teraa@gmail.com>
2025-10-11 08:07:49 -0300

    adjust to new fields spec

--- pages/src/data-fields.js
+++ pages/src/data-fields.js
@@ -3,8 +3,8 @@ import('/pages/src/fields.mjs').then(module => {
     module.fields.push({
             group: 'Segments',
             id: 'el-segment-next',
-            value: x => x.segmentData.nextSegment.name ? (x.segmentData.nextSegment.displayName ?? x.segmentData.nextSegment.name) + ": " + x.segmentData.nextSegment.distanceToGo : '',
-            key: x => (x && x.athlete) ? '' : 'Next Segment',
-            unit: x => x.segmentData.nextSegment.distanceToGoUnits ? x.segmentData.nextSegment.distanceToGoUnits : ''
+            format: x => x.segmentData.nextSegment.name ? (x.segmentData.nextSegment.displayName ?? x.segmentData.nextSegment.name) + ": " + x.segmentData.nextSegment.distanceToGo : '',
+            shortName: x => (x && x.athlete) ? '' : 'Next Segment',
+            suffix: x => x.segmentData.nextSegment.distanceToGoUnits ? x.segmentData.nextSegment.distanceToGoUnits : ''
     })
 });
